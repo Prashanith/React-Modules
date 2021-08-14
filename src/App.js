fa6f8da8bf38326a788879dce918f9f327ffe730
prashanith007 <prashanith007@gmail.com>
2021-08-14 09:09:28 +0530

    Changes Made

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 
-import PropTest from './components/propTest';
+// import PropTest from './components/propTest';
 
 function App() {
+  const [obj, setobj] = useState({
+    kick:10,
+    win:20,
+  })
+  let str=true
   
   return (
-    <div className="App">       
-        <PropTest />      
-    </div>
+    
+      str
+      && <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus labore odio dolor soluta tempora minus nesciunt dignissimos consequatur molestias, nemo fugiat, cupiditate officia aspernatur ea, ducimus veniam vel libero quae!</p>
+    
   );
 }
 
--- src/components/propTest.js
+++ src/components/propTest.js
@@ -1,32 +1,75 @@
 import React, { useState,useEffect } from 'react'
 
 function PropStub() {
-    const [number, setnumber] = useState(0);  
+    const [abc, setabc] = useState(0);
+    const [number, setnumber] = useState([
+        {
+            name:'Ney',
+            jersey:11
+        },
+        {
+            name:'Busi',
+            jersey:5
+        },        
+        {
+            name:'Messi',
+            jersey:10
+        },
+        {
+            name:'Ronnie',
+            jersey:7
+        },
+        {
+            name:'KDB',
+            jersey:17
+        },
+    ]);  
 
-    function increment() {
-        setnumber(number+1);    
+    function sortJersey(a,b) {
+        return a.jersey-b.jersey;        
+    }
+
+    function sortObj() {
+        setabc(abc+1);
+
+        let b=number;
+        
+        b.sort(sortJersey)
+        console.log(10,number);
+        setnumber(b);   
     }
 
     return (
-        <div>
-            <button onClick={()=>increment()}>Increment </button>
-            <br />
-            <h2>{number}</h2>
-            <PropTest number={number}/>      
+        <div >
+            <button onClick={()=>sortObj()}>Increment </button>
+            {abc}
+            <br />            
+            {
+                number.map((e)=>{
+                   return  <PropTest key={e.jersey} number={e}/> 
+                })
+            }                 
         </div>
     );
 }
 
 function PropTest(props) {
-    const [num, setnum] = useState(props.number)
-
     useEffect(() => {
-        setnum(props.number)
-    }, [props])
-
+        fetch('https://maps.googleapis.com/maps/api/distancematrix/json?origins=17.395007,78.510930&destinations=side_of_road:17.4135,78.5287&key=AIzaSyAgP6rjcTRM4-fVcxfGdgCGaaU6fW7f5xQ')
+        .then((e)=>{
+            console.log(e);
+        })
+    
+        
+    }, [])
     return (
-        <div>
-            <h2>{num}</h2>
+        <div style={{background:'grey'}}>
+            <h2>
+            { props.number.name }
+            </h2>
+            <h2>
+            { props.number.jersey }
+            </h2>
         </div>
     )
 }
